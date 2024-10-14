d30b2c7b2830463aab8bae26ada05bfc5a028978
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2024-10-14 20:20:09 +0500

    pricing page chagnes

--- src/app/pricing/packages.jsx
+++ src/app/pricing/packages.jsx
@@ -14,86 +14,91 @@ export default function Packages() {
         <section className={`${styles.PrincingSec} p-100`}>
             <Container >
                 <Row className={styles.PrincingContent}>
-                    <Col xl={6} lg={6} md={6} className={styles.PrincingContentLeft}>
-                        <span>{data?.subtitle}</span>
-                        <div className={styles.heading}>{data?.title}</div>
-                        <p>{data?.content}</p>
+                    <Col xl={6} lg={6} md={6} >
+                        <div className={styles.PrincingContentLeft}>
+                            <div className="tagTitle">{data?.subtitle}</div>
+                            <h2>{data?.title}</h2>
+                            <p>{data?.content}</p>
+                        </div>
                     </Col>
-                    <Col xl={6} lg={6} md={6} className={styles.PrincingContentRight}>
-                        <span>Select Service</span>
-                        <select name="menu" id="meun-items" className={styles.PrincingSelect}>
-                            <option disabled selected>2D Animation</option>
-                            <option value="3D Animation">3D Animation</option>
-                            <option value="Whiteboard Animation">Whiteboard Animation</option>
-                            <option value="Motion Graphics">Motion Graphics</option>
-                            <option value="Video Editing">Video Editing</option>
-                            <option value="Logo Animation">Logo Animation</option>
-                            <option value="Architectural Visualization">Architectural Visualization</option>
-                            <option value="CGI-VFX">CGI-VFX</option>
-                            <option value="Infographics">Infographics</option>
-                            <option value="Hybrid & Cel">Hybrid & Cel</option>
-                        </select>
+                    <Col xl={6} lg={6} md={6} >
+                        <div className={styles.PrincingContentRight}>
+                            <span>Select Service</span>
+                            <select name="menu" id="meun-items" className={styles.PrincingSelect}>
+                                <option disabled selected>2D Animation</option>
+                                <option value="3D Animation">3D Animation</option>
+                                <option value="Whiteboard Animation">Whiteboard Animation</option>
+                                <option value="Motion Graphics">Motion Graphics</option>
+                                <option value="Video Editing">Video Editing</option>
+                                <option value="Logo Animation">Logo Animation</option>
+                                <option value="Architectural Visualization">Architectural Visualization</option>
+                                <option value="CGI-VFX">CGI-VFX</option>
+                                <option value="Infographics">Infographics</option>
+                                <option value="Hybrid & Cel">Hybrid & Cel</option>
+                            </select>
+                        </div>
                     </Col>
                 </Row>
 
                 <div className={styles.pricing}>
-
                     <div className={styles.pricingBox}>
                         <div className={styles.box}>
-                            <span className={styles.package}>Basic Package</span>
+                            <div className={styles.packageName}>Basic Package</div>
                             <h4>$500.00</h4>
                             <p>We have something in store for everyone, and that something is affordable services</p>
-                            <button className={styles.button}>Get Started</button>
+                            <div className={styles.buttonCta}>Get Started</div>
                         </div>
-
                         <ul className={styles.pricingList}>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
+                            <li><PackageTick /> 30 Seconds Duration</li>
+                            <li><PackageTick /> Script Writing</li>
+                            <li><PackageTick /> Professional Voice-over & SFX</li>
+                            <li><PackageTick /> Hand-drawn Illustrations</li>
+                            <li><PackageTick /> Unlimited Revisions</li>
+                            <li><PackageTick /> HD Format Video</li>
+                            <li><PackageTick /> Dedicated Support</li>
                         </ul>
                     </div>
 
                     <div className={styles.pricingBox}>
                         <div className={styles.box}>
-                            <span className={styles.package}>Basic Package</span>
+                            <div className={styles.packageName}>Basic Package</div>
                             <h4>$500.00</h4>
                             <p>We have something in store for everyone, and that something is affordable services</p>
-                            <button className={styles.button}>Get Started</button>
+                            <div className={styles.buttonCta}>Get Started</div>
                         </div>
 
                         <ul className={styles.pricingList}>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
+                            <li><PackageTick /> 30 Seconds Duration</li>
+                            <li><PackageTick /> Script Writing</li>
+                            <li><PackageTick /> Professional Voice-over & SFX</li>
+                            <li><PackageTick /> Hand-drawn Illustrations</li>
+                            <li><PackageTick /> Unlimited Revisions</li>
+                            <li><PackageTick /> HD Format Video</li>
+                            <li><PackageTick /> Dedicated Support</li>
                         </ul>
                     </div>
 
                     <div className={styles.pricingBox}>
                         <div className={styles.box}>
-                            <span className={styles.package}>Basic Package</span>
+                            <div className={styles.packageName}>Basic Package</div>
                             <h4>$500.00</h4>
                             <p>We have something in store for everyone, and that something is affordable services</p>
-                            <button className={styles.button}>Get Started</button>
+                            <div className={styles.buttonCta}>Get Started</div>
                         </div>
 
                         <ul className={styles.pricingList}>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
-                            <li> <PackageTick /> 30sec Duration</li>
+                            <li><PackageTick /> 30 Seconds Duration</li>
+                            <li><PackageTick /> Script Writing</li>
+                            <li><PackageTick /> Professional Voice-over & SFX</li>
+                            <li><PackageTick /> Hand-drawn Illustrations</li>
+                            <li><PackageTick /> Unlimited Revisions</li>
+                            <li><PackageTick /> HD Format Video</li>
+                            <li><PackageTick /> Dedicated Support</li>
                         </ul>
                     </div>
 
                 </div>
             </Container>
-        </section>
+        </section >
     )
 }
