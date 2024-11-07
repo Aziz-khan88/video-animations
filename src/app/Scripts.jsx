4b71229c1503833c8eb50c888d9a23265329fbbc
cl-farooqaziz <dev.faz@productions-dev.com>
2024-11-08 03:32:22 +0500

    setTimeOut removed

--- src/app/Scripts.jsx
+++ src/app/Scripts.jsx
@@ -1,23 +1,21 @@
 "use client"
 import { useEffect } from 'react'
+
 const Scripts = () => {
     useEffect(() => {
-        const timer = setTimeout(() => {
-            //======== Live Chat ========//
-            window.__lc = window.__lc || {};
-            window.__lc.license = 18818250;
-            window.__lc.integration_name = "manual_onboarding";
-            window.__lc.product_name = "livechat";
-            const livechat = document.createElement("script");
-            livechat.async = true;
-            livechat.src = "https://cdn.livechatinc.com/tracking.js";
-            document.body.appendChild(livechat);
-        }, 3000);
-        return () => clearTimeout(timer);
+        //======== Live Chat ========//
+        window.__lc = window.__lc || {};
+        window.__lc.license = 18818250;
+        window.__lc.integration_name = "manual_onboarding";
+        window.__lc.product_name = "livechat";
+
+        const livechat = document.createElement("script");
+        livechat.async = true;
+        livechat.src = "https://cdn.livechatinc.com/tracking.js";
+        document.body.appendChild(livechat);
     }, []);
 
-    return (
-        <></>
-    );
+    return null;
 }
+
 export default Scripts
